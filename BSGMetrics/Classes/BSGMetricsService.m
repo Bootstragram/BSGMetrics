f1bc49148529c3dba649f3ecd97ddc18bcb1fe3b
Mickaël Floc’hlay <mickael@flochlay.com>
2016-06-14 21:34:18 +0200

    Added extra information #1

--- BSGMetrics/Classes/BSGMetricsService.m
+++ BSGMetrics/Classes/BSGMetricsService.m
@@ -41,6 +41,10 @@
     return @{
             @"createdAt": [_dateFormatter stringFromDate:event.createdAt],
             @"appVersion": event.version,
+            @"localeIdentifier": [NSLocale currentLocale].localeIdentifier,
+            @"deviceSystemName": [[UIDevice currentDevice] systemName],
+            @"deviceSystemVersion": [[UIDevice currentDevice] systemVersion],
+            @"deviceModel": [[UIDevice currentDevice] model],
             @"info": event.userInfo
             };
 }
